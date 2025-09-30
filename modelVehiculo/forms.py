b58afae3b5acd0053f497e444a31a06cb6d5e145
rodoflo-code1 <rodolfo.gllegos.1994@gmail.com>
2025-09-30 20:08:11 -0300

    creado proyecto

--- modelVehiculo/forms.py
+++ modelVehiculo/forms.py
@@ -1,38 +1,27 @@
+
 from django import forms
 from django.core import validators
-from modelVehiculo.models import Vehiculo
+from .models import Vehiculo
 class VehiculoForm(forms.ModelForm):
     class Meta:
         model = Vehiculo
-        fields = ['marca', 'modelo', 'año', 'precio', 'color', 'tipo_combustible']
+        fields = ['marca', 'modelo', 'año',  'color', 'combustible']
         
         # Aplicar estilos Bootstrap a todos los campos
         widgets = {
             'marca': forms.TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Ej: Toyota, Ford, Chevrolet'
-            }),
-            'modelo': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Ej: Corolla, Mustang, Camaro'
-            }),
-
-            'patente': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Ej: ABCD12',
-                'style': 'text-transform: uppercase;'
-            }),
-            'precio': forms.NumberInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Ej: 15000000',
-                'step': '0.01'
-            }),
-            'color': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Ej: Rojo, Azul, Negro'
-            }),
-            'tipo_combustible': forms.Select(attrs={
-                'class': 'form-select'
-            }),
+            })
         }
     
+
+    
+    def clean_marca(self):
+        """Validación para marca: mínimo 2 caracteres"""
+        marca = self.cleaned_data.get('marca')
+        if len(marca) < 2:
+            raise forms.ValidationError("La marca debe tener al menos 2 caracteres")
+        return marca.title()  # Capitalizar primera letra
+    
+   
