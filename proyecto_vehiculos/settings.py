afb83be0fb37feb8daac050d62e6f6b11522fb94
unknown <felipemoralesmejias@gmail.com>
2026-04-12 18:30:56 -0400

    nuevo

--- proyecto_vehiculos/settings.py
+++ proyecto_vehiculos/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['100.49.2.11']
 
 
 # Application definition
